54eed7263baf2f9637f34d1af971c629c569c076
MdAnassiddiqui <anas8933ald9@gmail.com>
2024-03-29 20:24:30 -0700

    Fully Function using mern

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,15 +1,15 @@
-const requireUser = require('../middlewares/requireUser');
-const UserController = require('../controllers/userController');
-const router = require('express').Router();
+const requireUser= require('../middlewares/requireUser');
+const UserController =require('../controllers/userController');
+const router =require('express').Router();
 
-router.post('/follow', requireUser, UserController.followOrUnfollowUserController);
-router.get('/getFeedData', requireUser, UserController.getPostsOfFollowing);
-router.get('/getMyPosts', requireUser, UserController.getMyPosts);
-router.get('/getUserPosts', requireUser, UserController.getUserPosts);
-router.delete('/', requireUser, UserController.deleteMyProfile);
-router.get('/getMyInfo', requireUser, UserController.getMyInfo);
+router.post("/follow",requireUser ,UserController.followOrUnfollowUserController);
 
-router.put('/', requireUser, UserController.updateUserProfile);
-router.post('/getUserProfile', requireUser, UserController.getUserProfile);
-
-module.exports = router;
+router.get('/getFeedData' ,requireUser , UserController.getPostsOfFollowing);
+router.get('/getMyPosts' ,requireUser , UserController.getMyPosts);
+router.get('/getUserPost' ,requireUser, UserController.getUserPosts);
+router.delete('/' ,requireUser , UserController.deleteMyProfile);
+router.get('/getMyInfo' ,requireUser ,UserController.getMyPosts);
+router.put('/' ,requireUser ,UserController.updateUserProfile);
+router.post('/getUserProfile' ,requireUser ,UserController.getUserProfile);
+router.delete('/deleteUser' ,requireUser , UserController.deleteUser)
+module.exports=router;
